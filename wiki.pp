0ee58f1f5eb7c6a31858634b0c38c4fc79f0ed13
Emile Calixte <calixte.emile@gmail.com>
2021-06-29 09:46:19 +0200

    Name instructions

--- wiki.pp
+++ wiki.pp
@@ -7,22 +7,26 @@ package {
         ensure => present
 }
 file {
-    '/usr/src/dokuwiki.tgz':
+    'download dokuwiki':
+        path => '/usr/src/dokuwiki.tgz',
         ensure => present,
         source => 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz'
 }
 exec {
-    'tar xavf dokuwiki.tgz':
+    'extract dokuwiki':
+        command => 'tar xavf dokuwiki.tgz',
         cwd => '/usr/src',
         path => ['/usr/bin', '/usr/sbin']
 }
 file {
-    '/usr/src/dokuwiki':
+    'rename dokuwiki':
+        path => '/usr/src/dokuwiki',
         ensure => present,
         source => '/usr/src/dokuwiki-2020-07-29'
 }
 file {
-    '/usr/src/dokuwiki-2020-07-29':
+    'delete extracted dokuwiki':
+        path => '/usr/src/dokuwiki-2020-07-29',
         ensure => absent
 }
 #file {
